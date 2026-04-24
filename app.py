9301882c4c0249b8bef16d358d961b72ca56e7cd
Amarhadpad <amarhadpad0@gmail.com>
2026-04-24 18:43:18 +0530

    change in app.run

--- app.py
+++ app.py
@@ -50,5 +50,7 @@ def predict():
         print("ERROR:", str(e))
         return jsonify({"error": str(e)})
 
+
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
